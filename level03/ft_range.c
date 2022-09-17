db17348f26e91eb3d1302c0dd80b7d7a5fa2c310
rschlott <rschlott@student.42wolfsburg.de>
2022-09-17 15:20:35 +0200

    new

--- level03/ft_range.c
+++ level03/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 22:07:49 by rschlott          #+#    #+#             */
-/*   Updated: 2022/09/17 13:30:04 by rschlott         ###   ########.fr       */
+/*   Updated: 2022/09/17 15:20:24 by rschlott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Allowed functions: malloc
 
 Write the following function:
 
-int     *ft_range(int start, int end);
+int	*ft_range(int start, int end);
 
 It must allocate (with malloc()) an array of integers, fill it with consecutive
 values that begin at start and end at end (Including start and end !), then
@@ -31,53 +31,87 @@ Examples:
 - With (0, -3) you will return an array containing 0, -1, -2 and -3.
 */
 
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-int *ft_range(int start, int end)
+int	*start_small(int start, int end, int length, int *ptr)
 {
-    int length;
-    int *ptr;
-    int begin;
-    int i;
+	int	i;
+	int	begin;
 
-    if (end > start)
-        length = end - start;
-    else if (start == end)
-        length = start - end;
-    else
-        length = start - end;
-    ptr = (int*) malloc((length + 1) * sizeof(int));
-    i = 0;
-    if (ptr == NULL)
-        return (NULL);
-    if (start < end)
-    {
-        begin = start - 1;
-        while (begin++ < end && i <= length)
-        {
-            ptr[i] = begin;
-            i++;
-        }
-    }
-    if (start > end)
-    {
-        begin = start + 1;
-        while (begin-- > end && i <= length)
-        {
-            ptr[i] = begin;
-            i++;
-        }
-    }       
-    return (ptr);
+	i = 0;
+	begin = start - 1;
+	while (begin++ < end && i <= length)
+	{
+		ptr[i] = begin;
+		i++;
+	}
+	return (ptr);
 }
+
+int	*start_big(int start, int end, int length, int *ptr)
+{
+	int	i;
+	int	begin;
+
+	i = 0;
+	begin = start + 1;
+	while (begin-- > end && i <= length)
+	{
+		ptr[i] = begin;
+		i++;
+	}
+	return (ptr);
+}
+
+int	*ft_range(int start, int end)
+{
+	int	length;
+	int	*ptr;
+
+	if (end > start)
+		length = end - start;
+	else if (start == end)
+		length = start - end;
+	else
+		length = start - end;
+	ptr = (int *)malloc((length + 1) * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	if (start < end)
+		ptr = start_small(start, end, length, ptr);
+	if (start > end)
+		ptr = start_big(start, end, length, ptr);
+	return (ptr);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	int	start;
+	int	end;
+	int	i;
+	int	*ptr;
+
+	start = -1;
+	end = 3;
+	i = 0;
+	ptr = ft_range(start, end);
+	while (i < 5)
+	{
+		printf("%d, ", ptr[i]);
+		i++;
+	}
+	return (0);
+}*/
 
 /*int main()
 {
-	int start;
-	int end;
+	int	start;
+	int	end;
     int i;
-	int *ptr;
+	int	*ptr;
 
     start = 0;
     end = -3;
@@ -91,24 +125,3 @@ int *ft_range(int start, int end)
     printf("%d\n", ptr[i]);
 	return (0);
 }*/
-
-#include <stdio.h>
-
-int main(void)
-{
-    int start;
-    int end;
-    int *ptr;
-    int i;
-
-    start = -1;
-    end = 2;
-    i = 0;
-    ptr = ft_range(start, end);
-    while (i < 4)
-    {
-        printf("%d, ", ptr[i]);
-        i++;
-    }        
-    return (0);   
-}
