a5e2f37f1a740d5d3c79139fcbc161186d481bad
rschlott <rschlott@student.42wolfsburg.de>
2022-09-17 13:25:44 +0200

    new

--- level03/ft_range.c
+++ level03/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 22:07:49 by rschlott          #+#    #+#             */
-/*   Updated: 2022/09/16 22:26:50 by rschlott         ###   ########.fr       */
+/*   Updated: 2022/09/17 13:25:34 by rschlott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,44 +31,85 @@ Examples:
 - With (0, -3) you will return an array containing 0, -1, -2 and -3.
 */
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
 int *ft_range(int start, int end)
 {
     int length;
     int *ptr;
     int begin;
+    int i;
 
-    length = end - start + 1;
-    if (length < 0)
-        length = length * (-1);
-    if (start == end)
-        length = 1;
-    ptr = (int *)malloc(length * sizeof(int));
-    if (ptr == NULL)
-        return (NULL)
+    if (end > start)
+        length = end - start;
+    else if (start == end)
+        length = start - end;
     else
+        length = start - end;
+    ptr = (int*) malloc((length + 1) * sizeof(int));
+    i = 0;
+    if (ptr == NULL)
+        return (NULL);
+    if (start < end)
     {
-        begin = start;
-        while (begin <= end)
+        begin = start - 1;
+        while (begin++ < end && i <= length)
         {
-            ptr = &begin;
-            beginn++;
+            ptr[i] = begin;
+            i++;
+            printf("begin %d\n", begin);
         }
     }
+    if (start > end)
+    {
+        begin = start + 1;
+        while (begin-- > end && i <= length)
+        {
+            ptr[i] = begin;
+            i++;
+            printf("begin %d\n", begin);
+        }
+    }       
     return (ptr);
 }
 
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdlib.h>
+int main()
+{
+	int start;
+	int end;
+    int i;
+	int *ptr;
 
-int main(void)
+    start = 0;
+    end = -3;
+	i = 0;
+	ptr = ft_range(start, end);
+	while (ptr[i] != end)
+	{
+		printf("%d\n", ptr[i]);
+		i++;
+	}
+    printf("%d\n", ptr[i]);
+	return (0);
+}
+
+/*int main(void)
 {
     int start;
     int end;
     int *ptr;
+    int i;
 
-    start = 1;
-    end = 5;
-    ptr = ft_range(start, end)
-    printf("%p", ptr);
-}
+    start = 0;
+    end = -3;
+    i = 0;
+    ptr = ft_range(start, end);
+    while (ptr[i] < 4)
+    {
+        printf("%d, ", ptr[i]);
+        i++;
+    }        
+    return (0);   
+}*/
