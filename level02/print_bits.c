6136caf297396307651a0e0f6a7c3823bdd215ee
rschlott <rschlott@student.42wolfsburg.de>
2022-09-18 14:36:35 +0200

    new

--- level02/print_bits.c
+++ level02/print_bits.c
@@ -6,7 +6,7 @@
 /*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 19:29:31 by rschlott          #+#    #+#             */
-/*   Updated: 2022/09/16 08:47:37 by rschlott         ###   ########.fr       */
+/*   Updated: 2022/09/18 14:36:26 by rschlott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,22 @@ Your function must be declared as follows:
 
 void	print_bits(unsigned char octet);
 
-Example, if you pass 2 to print_bits, it will print "00000010"
+Example, if you pass 2 to print_bits, it will print "00000010"; 1 is "00000001"; 0 is "00000000"
+
 */
 
-void    print_bits(unsigned char octet)
-{
-    
-}
 
+/*
+<< is the left shift operator. It is shifting the number 1 to the left 0 bits, which is equivalent to the number 1.
+The reason that they can be combined together without interfering with each other is that each one is a power of two, 
+and that is the reason for using 1 << x, because that yields powers of two:
+1 << 0 == 2^0 == 1 == binary 00000001
+1 << 1 == 2^1 == 2 == binary 00000010
+1 << 2 == 2^2 == 4 == binary 00000100
+1 << 3 == 2^3 == 8 == binary 00001000
+etc
+*/
 
-/*#include <stdio.h>
 #include <unistd.h>
 
 void print_bits(unsigned char octet)
@@ -55,10 +61,11 @@ void print_bits(unsigned char octet)
 	}
 }
 
-int main()
+int main(void)
 {
-	unsigned char chr = 2;
-
+	unsigned char chr;
+	
+	chr = 8;
 	print_bits(chr);
 	return (0);
-}*/
+}
