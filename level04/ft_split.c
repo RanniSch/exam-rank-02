202cb85a25f3ebf6435cdce497a51a0ac8642546
rschlott <rschlott@student.42wolfsburg.de>
2022-09-19 08:58:24 +0200

    new

--- level04/ft_split.c
+++ level04/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: rschlott <rschlott@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 21:53:19 by rschlott          #+#    #+#             */
-/*   Updated: 2022/09/17 21:59:45 by rschlott         ###   ########.fr       */
+/*   Updated: 2022/09/19 08:57:56 by rschlott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,91 @@ Your function must be declared as follows:
 
 char    **ft_split(char *str);
 */
+
+#include <stdlib.h>
+
+int	ft_wordlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+		++i;
+	return (i);
+}
+
+char	*word_dupe(char *str)
+{
+	int i = 0;
+	int len = ft_wordlen(str);
+	char *word = malloc(sizeof(char) * (len + 1));
+	
+	word[len] = '\0';
+	while (i < len)
+	{
+		word[i] = str[i];
+		++i;
+	}
+	return (word);
+}
+
+int		count_words(char *str)
+{
+	int num_words = 0;
+	
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+	while (*str != '\0')
+	{
+		num_words++;
+		while (*str != '\0' && *str != ' ' && *str != '\t' && *str != '\n')
+			str++;
+		while (*str == ' ' || *str == '\t' || *str == '\n')
+			str++;
+	}
+	return (num_words);
+}
+
+void	fill_words(char **array, char *str)
+{
+	int word_index = 0;
+	
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		++str;
+	while (*str != '\0')
+	{
+		array[word_index] = word_dupe(str);
+		++word_index;
+		while (*str != '\0' && *str != ' ' && *str != '\t' && *str != '\n')
+			++str;
+		while (*str == ' ' || *str == '\t' || *str == '\n')
+			++str;
+	}
+}
+
+char	**ft_split(char *str)
+{
+	int		num_words;
+	char	**array;
+	
+	num_words = count_words(str);
+	array = malloc(sizeof(char *) * (num_words + 1));
+	array[num_words] = 0;
+	fill_words(array, str);
+	return (array);
+}
+
+/*#include <stdio.h>
+
+int		main(void)
+{
+	int i = 0;
+	char **tab;
+		
+	tab = ft_split("bonjour je m'appel Arthur");
+	while (i < 4)
+	{
+		printf("string %d : %s\n", i, tab[i]);
+		i++;
+	}
+	return (0);
+}*/
